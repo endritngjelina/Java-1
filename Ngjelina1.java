41b922076e8747fd7d06c2bd8859bbbb356c8e2a
Endrit Ngjelina <121914667+endritngjelina@users.noreply.github.com>
2025-04-06 19:51:51 -0400

    Add files via upload

---
+++ Ngjelina1.java
@@ -0,0 +1,23 @@
+package ngjelina1and2;
+//Endrit Ngjelina
+//SPC ID#2436798
+//Worked Alone
+//This program prints my name
+//It prints the name of the founder of Java
+//It prints "Java Rocks"
+//It prints the answer of the calculation
+
+public class Ngjelina1 {
+
+	public static void main(String[] args) {
+//Display my name
+		System.out.println("Endrit Ngjelina");
+//Displays Java's founder name
+		System.out.println("James Gosling");
+//Displays "Java rocks"
+		System.out.println("Java rocks!");
+//Displays the answer to the calculation
+		System.out.println((12.5 + 5.5 / 3) / (6.25 * 6 - 5.0));
+	}
+
+}
